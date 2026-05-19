0ac431f5760371c6618a65fd64a765c91101e445
Dyrox <dyhmusicstuff@gmail.com>
2026-05-19 20:44:09 +0100

    chore(firebase-config): change the firebase into dyrox

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -1,13 +1,12 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDYHw9oVZ84207EQYobb7DcxYHJAeazov8",
-  authDomain: "texnique-v3.firebaseapp.com",
-  projectId: "texnique-v3",
-  storageBucket: "texnique-v3.firebasestorage.app",
-  messagingSenderId: "59805088347",
-  appId: "1:59805088347:web:f780a0440e4e78d0c1dd7e",
-  measurementId: "G-B052LGHXCW"
+  apiKey: "AIzaSyAr5sYLDMqbKLsZXUrBiE8-1TnCaTebYbo",
+  authDomain: "texniques-1be98.firebaseapp.com",
+  projectId: "texniques-1be98",
+  storageBucket: "texniques-1be98.firebasestorage.app",
+  messagingSenderId: "541754954306",
+  appId: "1:541754954306:web:2da5133b7c1e6d86c7d3db"
 };
 
 // Initialize Firebase
